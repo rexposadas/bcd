87ed95f372ed59e0d61e6e574e7f92d6e7efdb56
Rex Posadas <rexposadas@gmail.com>
2023-06-28 16:05:29 +0800

    assignment

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
+// todo: be able to add the numbers in the list
 func add(x, y int) int {
 	return x + y
+}
+
+// todo: concatenate the strings in the list
+func concat(list []string) {
 
 }
 
 func main() {
 
-	a := add(1, 2)
+	// todo: implement all this
+	list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	a := add()
 	fmt.Println("sum is ", a)
+
+	stringList := []string{"hello", "world", "this", "is", "a", "list", "of", "strings"}
+	result := concat()
+	// output: "hello world this is a list of strings"
+	fmt.Println(result)
 
 }
