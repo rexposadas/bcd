bc155dac8a7f391740b65a25e2a577f65ff09601
Rex Posadas <rexposadas@gmail.com>
2023-07-01 00:52:44 +0800

    added new assignments

--- utils/example.go
+++ utils/example.go
@@ -1,5 +1,14 @@
 package utils
 
+// structs <-- objects java and c++
+// objects: data structures which has fields and methods
+
+type M struct {
+}
+
+// TODO: make the functions below as receivers for struct M
+// TODO: modify the main.go file accordingly.
+
 func Concat(strings []string) string {
 	concatenated := ""
 	for a, str := range strings {
