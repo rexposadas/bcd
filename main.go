ff6535693388bc5b378a41fb8a8377052dae9314
Ariel <128343170+tanggol69@users.noreply.github.com>
2023-07-04 20:04:13 +0800

    Task/rposadas/rest api (#1)
    * created rest api
    
    * rest-api task
    
    ---------
    
    Co-authored-by: Rex Posadas <rexposadas@gmail.com>

--- main.go
+++ main.go
@@ -1,17 +1,80 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/rexposadas/bcd/utils"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
-	numberList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	a := utils.Add(numberList)
-	fmt.Println("sum is", a, ".")
+	port := "8080"
 
-	stringList := []string{"hello", "world", "this", "is", "a", "list", "of", "strings"}
-	concatenate := utils.Concat(stringList)
-	fmt.Println(concatenate)
+	if p := os.Getenv("PORT"); p != "" {
+		if _, err := strconv.Atoi(p); err == nil {
+			port = p
+		}
+	}
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+		})
+	})
+
+	r.POST("/concat", func(c *gin.Context) {
+		req := c.Request
+
+		b, err := io.ReadAll(req.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		payload := struct {
+			Data []string `json:"data"`
+		}{}
+
+		if err := json.Unmarshal(b, &payload); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		m := utils.M{}
+		result := m.Concat(payload.Data)
+
+		c.JSON(http.StatusOK, gin.H{
+			"result": result,
+		})
+	})
+ 
+	r.POST("/add", func(c *gin.Context) {
+		req := c.Request
+		b, err := io.ReadAll(req.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+	}
+	payload := struct {
+		Data []int `json:"data"`
+	}{}
+	if err := json.Unmarshal(b, &payload); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+}
+	m := utils.M{}
+	result := m.Add(payload.Data)
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": result,
+	})
+})
+
+r.Run(fmt.Sprintf(":%s", port))
+
 }
